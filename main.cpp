1f97b1ccf222468633df8757daa7fac10a002961
drtc <tc.tjc.irvine@gmail.com>
2020-10-26 20:45:24 -0700

    + double array size when stack overflow

--- main.cpp
+++ main.cpp
@@ -24,13 +24,16 @@ public:
     {
         // There is at least one empty space for new element
         if (top1 < top2 - 1) {
-            top1++;
-            arr[top1] = x;
+            // top1++;
+            // arr[top1] = x;
         }
         else {
-            cout << "Stack Overflow";
-            exit(1);
+            cout << "Stack Overflow! Doubling Array size." << endl;
+            doubleStackArray();
+            // exit(1);
         }
+        top1++;
+        arr[top1] = x;
     }
 
     // Method to push an element x to stack2
@@ -39,14 +42,16 @@ public:
         // There is at least one empty
         // space for new element
         if (top1 < top2 - 1) {
-            top2--;
-            arr[top2] = x;
+            // top2--;
+            // arr[top2] = x;
         }
         else {
-            cout << "Stack Overflow";
+            cout << "Stack Overflow! Doubling Array size." << endl;
+            doubleStackArray();
             // exit(1);
-
         }
+        top2--;
+        arr[top2] = x;
     }
 
     // Method to pop an element from first stack
@@ -99,6 +104,25 @@ public:
         cout << "size 1: " << size_first() << endl;
         cout << "size 2: " << size_second() << endl;
     }
+
+    void doubleStackArray()
+    {
+        int old_size = size;
+        int new_size = 2*size;
+        int* newArr = new int[new_size];
+        // initialize array
+        for (int i=0; i<new_size; i++) newArr[i] = 0;
+        // copy stack 1
+        for (int i=0; i<=top1; i++) newArr[i] = arr[i];
+        // copy stack 1
+        for (int i=1; i<= size_second() ; i++) newArr[new_size - i] = arr[old_size - i];
+        // delete old array
+        delete[] arr;
+        // original array pointer to point to new array
+        arr = newArr;
+        size = new_size;
+        top2 += old_size;
+    }
 };
 
 /* Driver program to test twStacks class */
@@ -106,7 +130,9 @@ int main()
 {
     try {
         twoStacksOneArray ts(5);
+        ts.printArray();
         ts.push_first(5);
+        cout << "stk1 -> push 5" << endl;
         ts.printArray();
         /*
         cout << "Popped element from stack1 is "
@@ -115,22 +141,31 @@ int main()
              << ts.pop_first() << endl;
         */
         ts.push_second(10);
+        cout << "stk2 -> push 10" << endl;
         ts.printArray();
         ts.push_second(15);
+        cout << "stk2 -> push 15" << endl;
         ts.printArray();
         ts.push_first(11);
+        cout << "stk1 -> push 11" << endl;
         ts.printArray();
         ts.push_second(7);
+        cout << "stk2 -> push 7" << endl;
         ts.printArray();
-        ts.push_second(100);
+        ts.push_first(100);
+        cout << "stk1 -> push 100" << endl;
+        //ts.push_second(100);
+        // cout << "stk2 -> push 100" << endl;
         ts.printArray();
+        /*
         cout << "Popped element from stack1 is "
              << ts.pop_first() << endl;
         ts.push_second(40);
         ts.printArray();
         cout << "\nPopped element from stack2 is "
              << ts.pop_second() << endl;
-        ts.printArray();
+        */
+        // ts.printArray();
     } catch (out_of_range& excpt){
         cout << endl << "Out of Range" << endl;
         cout << excpt.what() << endl;
