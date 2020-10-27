0b27c296f80c803604c9e962e1eaece74afc60d6
drtc <tc.tjc.irvine@gmail.com>
2020-10-26 17:43:24 -0700

    + size_first, size_second, underflow out of range exception

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ public:
     }
 
     // Method to push an element x to stack1
-    void push1(int x)
+    void push_first(int x)
     {
         // There is at least one empty space for new element
         if (top1 < top2 - 1) {
@@ -33,7 +33,7 @@ public:
     }
 
     // Method to push an element x to stack2
-    void push2(int x)
+    void push_second(int x)
     {
         // There is at least one empty
         // space for new element
@@ -43,12 +43,13 @@ public:
         }
         else {
             cout << "Stack Overflow";
-            exit(1);
+            // exit(1);
+
         }
     }
 
     // Method to pop an element from first stack
-    int pop1()
+    int pop_first()
     {
         if (top1 >= 0) {
             int x = arr[top1];
@@ -56,13 +57,14 @@ public:
             return x;
         }
         else {
-            cout << "Stack UnderFlow";
-            exit(1);
+            // cout << "Stack 1 UnderFlow";
+            throw out_of_range("First Stack UnderFlow");
+            // exit(1);
         }
     }
 
     // Method to pop an element from second stack
-    int pop2()
+    int pop_second()
     {
         if (top2 < size) {
             int x = arr[top2];
@@ -70,25 +72,50 @@ public:
             return x;
         }
         else {
-            cout << "Stack UnderFlow";
+            // cout << "Stack 2 UnderFlow";
+            throw out_of_range("Second Stack UnderFlow");
             exit(1);
         }
+    }
+
+    // Size of first stack
+    int size_first()
+    {
+        return (top1 - (-1) );
+    }
+
+    // Size of second stack
+    int size_second()
+    {
+        return ( size - top2 );
     }
 };
 
 /* Driver program to test twStacks class */
 int main()
 {
-    twoStacksOneArray ts(5);
-    ts.push1(5);
-    ts.push2(10);
-    ts.push2(15);
-    ts.push1(11);
-    ts.push2(7);
-    cout << "Popped element from stack1 is "
-         << ts.pop1();
-    ts.push2(40);
-    cout << "\nPopped element from stack2 is "
-         << ts.pop2();
+    try {
+        twoStacksOneArray ts(5);
+        ts.push_first(5);
+        cout << "size 1: " << ts.size_first() << endl;
+        cout << "size 2: " << ts.size_second() << endl;
+        cout << "Popped element from stack1 is "
+             << ts.pop_first() << endl;
+        cout << "Popped element from stack1 is "
+             << ts.pop_first() << endl;
+        ts.push_second(10);
+        ts.push_second(15);
+        ts.push_first(11);
+        ts.push_second(7);
+        cout << "Popped element from stack1 is "
+             << ts.pop_first() << endl;
+        ts.push_second(40);
+        cout << "\nPopped element from stack2 is "
+             << ts.pop_second() << endl;
+    } catch (out_of_range& excpt){
+        cout << endl << "Out of Range" << endl;
+        cout << excpt.what() << endl;
+        exit(1);
+    }
     return 0;
 }
